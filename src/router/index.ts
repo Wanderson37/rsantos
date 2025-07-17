67dd68c59f9198205db76b1f997df32e0a329bca
Wanderson37 <wanderson_c.o@hotmail.com>
2025-07-17 15:28:04 -0400

    Refactor code structure for improved readability and maintainability

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/layouts/MainLayout.vue'),
+      meta: { protected: true, transition: 'slideX' },
+      children: [
+        {
+          path: 'home',
+          alias: '',
+          component: () => import('@/views/HomeView.vue'),
+        },
+      ],
+    },
+  ],
+})
+
+export default router
