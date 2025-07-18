81e1c58918ea9d81de638ad7235b61174d06e487
Wanderson37 <wanderson_c.o@hotmail.com>
2025-07-18 09:30:22 -0400

    Add base styles, main layout, and component-specific styles
    - Introduced base styles in base.sass including font imports and global styles.
    - Created main.sass for layout and typography adjustments, including responsive design for various elements.
    - Added specific styles for Quasar components in quasar.sass, enhancing UI consistency and responsiveness.
    - Implemented transition effects in transitions.sass for smoother animations on component state changes.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,27 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>RSantos</q-toolbar-title>
+      <q-toolbar class="bg-grey-3">
+        <q-toolbar-title class="text-black text-center">
+          <q-avatar>
+            <img src="/public/rsantos_logo.png" alt="Logo" />
+          </q-avatar>
+          RSantos
+        </q-toolbar-title>
       </q-toolbar>
+      <q-tabs
+        v-model="tab"
+        class="bg-grey-3 text-black"
+        active-color="primary"
+        indicator-color="primary"
+      >
+        <q-tab name="home" label="Home" />
+        <q-tab name="quem_somos" label="Quem Somos" />
+        <q-tab name="servicos" label="Serviços" />
+        <q-tab name="noticias" label="Notícias" />
+        <q-tab name="fale_conosco" label="Fale Conosco" />
+        <q-tab name="ultimas_noticias" label="Últimas Notícias" />
+      </q-tabs>
     </q-header>
 
     <q-page-container>
@@ -11,3 +29,9 @@
     </q-page-container>
   </q-layout>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const tab = ref('home')
+</script>
