04808d2a7b0dda596593a530466463f2eacd05b4
Wanderson37 <wanderson_c.o@hotmail.com>
2025-07-19 16:55:41 -0400

    Add BaseButton component with template and script setup

---
+++ src/wrappers/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-btn v-bind="props" v-ripple>
+    <template v-for="(_, slot) in slots" :key="slot" v-slot:[slot]="scope">
+      <slot :name="slot" v-bind="scope" :key="slot" />
+    </template>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import type { QBtnProps } from 'quasar'
+import { useSlots } from 'vue'
+
+const slots = useSlots() as never
+
+const props = withDefaults(defineProps<QBtnProps>(), {
+  color: 'secondary',
+  noCaps: true,
+  unelevated: true
+})
+</script>
