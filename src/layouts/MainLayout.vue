6f06b44957ab5807ddf504abf5ff52f323bba111
Wanderson37 <wanderson_c.o@hotmail.com>
2025-07-21 08:18:10 -0400

    Refactor MainLayout component to enhance mobile navigation with a responsive drawer and improve toolbar layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,27 +2,94 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="bg-grey-3">
-        <q-toolbar-title class="text-black text-center">
-          <q-avatar>
-            <img src="/rsantos_logo.png" alt="Logo" />
-          </q-avatar>
-          RSantos
+        <q-btn
+          v-if="$q.screen.lt.sm"
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+          class="text-black"
+        />
+        <q-toolbar-title class="text-black">
+          <div class="row items-center q-gutter-sm">
+            <q-avatar>
+              <img src="/rsantos_logo.png" alt="Logo" />
+            </q-avatar>
+            <span>RSantos</span>
+          </div>
         </q-toolbar-title>
+        <q-tabs
+          v-if="!$q.screen.lt.sm"
+          v-model="tab"
+          class="text-black"
+          active-color="primary"
+          indicator-color="primary"
+        >
+          <q-route-tab name="home" label="Home" to="/home" />
+          <q-route-tab name="quem_somos" label="Quem Somos" to="/quem-somos" />
+          <q-route-tab name="servicos" label="Serviços" to="/servicos" />
+          <q-route-tab name="noticias" label="Notícias" to="/noticias" />
+          <q-route-tab name="fale_conosco" label="Fale Conosco" to="/fale-conosco" />
+          <q-route-tab name="ultimas_noticias" label="Últimas Notícias" to="/ultimas-noticias" />
+        </q-tabs>
       </q-toolbar>
-      <q-tabs
-        v-model="tab"
-        class="bg-grey-3 text-black"
-        active-color="primary"
-        indicator-color="primary"
-      >
-        <q-route-tab name="home" label="Home" to="/home" />
-        <q-route-tab name="quem_somos" label="Quem Somos" to="/quem-somos" />
-        <q-route-tab name="servicos" label="Serviços" to="/servicos" />
-        <q-route-tab name="noticias" label="Notícias" to="/noticias" />
-        <q-route-tab name="fale_conosco" label="Fale Conosco" to="/fale-conosco" />
-        <q-route-tab name="ultimas_noticias" label="Últimas Notícias" to="/ultimas-noticias" />
-      </q-tabs>
     </q-header>
+
+    <q-drawer
+      v-if="$q.screen.lt.sm"
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header>Menu</q-item-label>
+
+        <q-item clickable v-ripple to="/home" @click="leftDrawerOpen = false">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>Home</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple to="/quem-somos" @click="leftDrawerOpen = false">
+          <q-item-section avatar>
+            <q-icon name="info" />
+          </q-item-section>
+          <q-item-section>Quem Somos</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple to="/servicos" @click="leftDrawerOpen = false">
+          <q-item-section avatar>
+            <q-icon name="business" />
+          </q-item-section>
+          <q-item-section>Serviços</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple to="/noticias" @click="leftDrawerOpen = false">
+          <q-item-section avatar>
+            <q-icon name="article" />
+          </q-item-section>
+          <q-item-section>Notícias</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple to="/fale-conosco" @click="leftDrawerOpen = false">
+          <q-item-section avatar>
+            <q-icon name="contact_mail" />
+          </q-item-section>
+          <q-item-section>Fale Conosco</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple to="/ultimas-noticias" @click="leftDrawerOpen = false">
+          <q-item-section avatar>
+            <q-icon name="newspaper" />
+          </q-item-section>
+          <q-item-section>Últimas Notícias</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -34,4 +101,9 @@
 import { ref } from 'vue'
 
 const tab = ref('home')
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
 </script>
