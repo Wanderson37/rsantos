a6c201da1b204228ec13029f66028d32586ad21f
Wanderson37 <wanderson_c.o@hotmail.com>
2025-08-09 09:08:54 -0400

    Refactor MainLayout and update QuemSomosView with detailed content and improved layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -20,20 +20,22 @@
             <span>RSantos</span>
           </div>
         </q-toolbar-title>
-        <q-tabs
-          v-if="!$q.screen.lt.sm"
-          v-model="tab"
-          class="text-black"
-          active-color="primary"
-          indicator-color="primary"
-        >
-          <q-route-tab name="home" label="Home" to="/home" />
-          <q-route-tab name="quem_somos" label="Quem Somos" to="/quem-somos" />
-          <q-route-tab name="servicos" label="Serviços" to="/servicos" />
-          <q-route-tab name="noticias" label="Notícias" to="/noticias" />
-          <q-route-tab name="fale_conosco" label="Fale Conosco" to="/fale-conosco" />
-          <q-route-tab name="ultimas_noticias" label="Últimas Notícias" to="/ultimas-noticias" />
-        </q-tabs>
+        <Transition name="slideX">
+          <q-tabs
+            v-if="!$q.screen.lt.sm"
+            v-model="tab"
+            class="text-black"
+            active-color="primary"
+            indicator-color="primary"
+          >
+            <q-route-tab name="home" label="Home" to="/home" />
+            <q-route-tab name="quem_somos" label="Quem Somos" to="/quem-somos" />
+            <q-route-tab name="servicos" label="Serviços" to="/servicos" />
+            <q-route-tab name="noticias" label="Notícias" to="/noticias" />
+            <q-route-tab name="fale_conosco" label="Fale Conosco" to="/fale-conosco" />
+            <q-route-tab name="ultimas_noticias" label="Últimas Notícias" to="/ultimas-noticias" />
+          </q-tabs>
+        </Transition>
       </q-toolbar>
     </q-header>
 
--- src/views/QuemSomosView.vue
+++ src/views/QuemSomosView.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1 class="text-center">Em construção</h1>
+  <div class="column items-center">
+    <h1 class="fs48 q-my-lg text-center">Quem somos</h1>
+    <div class="fs20 q-mx-md text-justify" style="max-width: 800px">
+      <p>
+        Bem-vindo à RSantos Contabilidade, onde a excelência em contabilidade e consultoria
+        financeira se encontra com um compromisso inabalável com nossos clientes. Fundado em
+        13/01/2015, passamos por diversos desafios e reformulações que nos proporcionaram um
+        desenvolvimento sólido, sem perder a essência de nossa missão inicial de oferecer soluções
+        contábeis inovadoras e personalizadas. Nosso escritório é composto por uma equipe de
+        profissionais altamente qualificados e dedicados a proporcionar um serviço de qualidade
+        superior.
+      </p>
+
+      <p>
+        Com anos de experiência no mercado, nos destacamos pela nossa abordagem integrada e
+        estratégica, que abrange todas as áreas da contabilidade, desde a gestão financeira até o
+        planejamento tributário e estratégias societárias, entre outras expertises. Garantimos que
+        cada aspecto do seu negócio esteja otimizado para o sucesso, fornecendo uma visão clara e
+        precisa das finanças da sua empresa, permitindo que você tome decisões informadas e
+        estratégicas.
+      </p>
+
+      <p>
+        Acreditamos na importância da transparência, da confiança e do relacionamento próximo com
+        nossos clientes. Por isso, investimos continuamente em tecnologia e em capacitação
+        profissional, assegurando que estamos sempre à frente das mudanças e exigências do mercado.
+      </p>
+
+      <p>
+        Na RSantos Contabilidade, sua empresa encontra um parceiro confiável e comprometido com a
+        sua prosperidade. Junte-se a nós e descubra como podemos ajudar a transformar os desafios
+        contábeis em oportunidades de crescimento.
+      </p>
+    </div>
+  </div>
 </template>
