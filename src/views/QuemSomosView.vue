c0444343f5e0ab61ad1aecf8c023c1dbf07d39f9
Wanderson37 <wanderson_c.o@hotmail.com>
2025-08-09 11:09:36 -0400

    Enhance QuemSomosView with detailed mission, values, and principles sections; include highlights for improved readability

--- src/views/QuemSomosView.vue
+++ src/views/QuemSomosView.vue
@@ -33,5 +33,99 @@
         contábeis em oportunidades de crescimento.
       </p>
     </div>
+    <div
+      v-for="missao in missao"
+      :key="missao.index"
+      class="q-mt-md q-mb-xl"
+      style="max-width: 800px"
+    >
+      <h2 class="fs36 text-primary">{{ missao.title }}</h2>
+      <q-list separator class="q-pt-sm">
+        <q-item v-for="(it, i) in missao.items" :key="i" class="q-py-md">
+          <q-item-section v-if="!$q.screen.lt.sm" avatar>
+            <q-icon color="primary" name="check_circle" />
+          </q-item-section>
+          <q-item-section>
+            <div>
+              <div v-if="it.highlight">
+                <b class="fs20 text-primary">{{ it.highlight }}</b
+                >:
+
+                <span class="fs20">{{ it.text }}</span>
+              </div>
+              <div v-else>
+                <span class="fs20">{{ it.text }}</span>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+const missao = [
+  {
+    index: 1,
+    title: 'Missão',
+    items: [
+      {
+        highlight: '',
+        text: 'Nossa missão é oferecer soluções contábeis inovadoras e personalizadas, proporcionando serviços de qualidade superior que agreguem valor aos negócios de nossos clientes. Estamos comprometidos em fornecer orientação estratégica e suporte contínuo para otimizar a gestão financeira e tributária, contribuindo para o sucesso e crescimento sustentável das empresas que atendemos.',
+      },
+    ],
+  },
+  {
+    index: 2,
+    title: 'Valores',
+    items: [
+      {
+        highlight: 'Excelência',
+        text: 'Buscamos a excelência em tudo o que fazemos, garantindo a qualidade e precisão dos nossos serviços.',
+      },
+      {
+        highlight: 'Transparência',
+        text: 'Mantemos uma comunicação clara e aberta com nossos clientes, construindo relações de confiança mútua.',
+      },
+      {
+        highlight: 'Inovação',
+        text: 'Investimos continuamente em tecnologia e capacitação para oferecer soluções contábeis modernas e eficazes.',
+      },
+      {
+        highlight: 'Comprometimento',
+        text: 'Estamos dedicados ao sucesso de nossos clientes, atuando como parceiros estratégicos em todas as fases do negócio.',
+      },
+      {
+        highlight: 'Ética',
+        text: 'Agimos com integridade e responsabilidade, respeitando as normas e regulamentos profissionais.',
+      },
+    ],
+  },
+  {
+    index: 3,
+    title: 'Principios',
+    items: [
+      {
+        highlight: 'Foco no Cliente',
+        text: 'Nossos clientes estão no centro de tudo o que fazemos. Esforçamo-nos para entender suas necessidades e exceder suas expectativas.',
+      },
+      {
+        highlight: 'Qualidade e Precisão',
+        text: 'Garantimos a precisão dos nossos serviços e a qualidade das informações fornecidas, essencial para a tomada de decisões estratégicas.',
+      },
+      {
+        highlight: 'Desenvolvimento Contínuo',
+        text: 'Valorizamos o aprendizado contínuo e o aprimoramento profissional para nos mantermos atualizados e competitivos.',
+      },
+      {
+        highlight: 'Responsabilidade Social',
+        text: 'Contribuímos para o desenvolvimento sustentável das comunidades em que atuamos, promovendo práticas empresariais responsáveis.',
+      },
+      {
+        highlight: 'Trabalho em Equipe',
+        text: 'Acreditamos que a colaboração e o respeito mútuo entre nossa equipe e nossos clientes são fundamentais para alcançar resultados excepcionais.',
+      },
+    ],
+  },
+]
+</script>
